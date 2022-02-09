784eec25efa1042e7e6885b31b17b741d9807dc7
TimotheeGimbert <timothee.gimbert@free.fr>
2022-02-09 03:58:24 +0100

    darkTheme button and ThemeContext working

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import ThemeContext from '../../contexts/ThemeContext';
 
-const Navbar = () => (
-  <nav>
-    <Link to='/'>Accueil</Link> <br/>
-    <Link to='/about'>Agence</Link> <br/>
-    <Link to='/works'>Projects</Link> <br/>
-  </nav>
-);
+const Navbar = () => {
+  const darkTheme = useContext(ThemeContext);
+
+  return (
+    <nav className={darkTheme.darkTheme ? "dark" : "light"}>
+      <Link to='/'>Accueil</Link> <br/>
+      <Link to='/about'>Agence</Link> <br/>
+      <Link to='/works'>Projects</Link> <br/>
+      <button onClick={darkTheme.toggleTheme}>Changer le thème</button>
+    </nav>
+  )
+};
 
 export default Navbar;
