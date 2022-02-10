a4aeaf2c60cf5248b0e5337713593109eff49ff6
TimotheeGimbert <timothee.gimbert@free.fr>
2022-02-10 18:45:38 +0100

    adds bonus question

---
+++ src/contexts/CardsContext.js
@@ -0,0 +1,5 @@
+import { createContext } from 'react';
+
+const CardsContext = createContext(true);
+
+export default CardsContext;
--- src/pages/Works/index.jsx
+++ src/pages/Works/index.jsx
@@ -1,31 +1,38 @@
-import React, { useContext } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import ProjectCard from '../../components/ProjectCard';
 import Projects from '../../assets/projects.json';
-import ThemeContext from '../../contexts/ThemeContext';
+import CardsContext from '../../contexts/CardsContext';
 
 const Works = () => {
-  const theme = useContext(ThemeContext);
+  const [cardDisplay, setCardDisplay] = useState(true);
+
   return (
-    <main className='Works'>
-      <div className='hero'>
-        <h2>Au fil des années, nous accompagnons les meilleurs</h2>
-        <p>Découvrez pas à pas comment nous sommes présents pour lancer vos marques préférées</p>
-      </div>
-      {(!theme.darkTheme) && 
-      <section className='projects'>
-        <ProjectCard project={Projects['darwin']}/>
-        <ProjectCard project={Projects['tropcstraw']}/>
-        <ProjectCard project={Projects['kedal']}/>
-      </section>
-        || 
-      <section className='projects'>
-        <Link to={{ pathname: `/works/darwin-study-case` }}>Découvrir le projet Darwin</Link>
-        <Link to={{ pathname: `/works/tropcstraw-study-case` }}>Découvrir le projet TropCStraw</Link>
-        <Link to={{ pathname: `/works/kedal-study-case` }}>Découvrir le projet Kedal</Link>
-      </section>
-      }
-    </main>
+    <CardsContext.Provider value={{
+      cardDisplay,
+      toggleCards: () => setCardDisplay(cardDisplay => !cardDisplay)
+    }}>
+      <main className='Works'>
+        <div className='hero'>
+          <h2>Au fil des années, nous accompagnons les meilleurs</h2>
+          <p>Découvrez pas à pas comment nous sommes présents pour lancer vos marques préférées</p>
+        </div>
+        {(cardDisplay) && 
+        <section className='projects'>
+          <ProjectCard project={Projects['darwin']}/>
+          <ProjectCard project={Projects['tropcstraw']}/>
+          <ProjectCard project={Projects['kedal']}/>
+        </section>
+          || 
+        <section className='projects'>
+          <Link to={{ pathname: `/works/darwin-study-case` }}>Découvrir le projet Darwin</Link>
+          <Link to={{ pathname: `/works/tropcstraw-study-case` }}>Découvrir le projet TropCStraw</Link>
+          <Link to={{ pathname: `/works/kedal-study-case` }}>Découvrir le projet Kedal</Link>
+        </section>
+        }
+        <button onClick={() => setCardDisplay(!cardDisplay)}>Changer affichage des projets</button>
+      </main>
+    </CardsContext.Provider>
   )
 };
 
